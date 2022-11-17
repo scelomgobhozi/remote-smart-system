6e4fa11e5d834396b72f09d89ea56cdff3c8c1b6
Sicelo Mgobhozi <scelomgobhozi27@gmail.com>
2022-11-18 01:27:10 +0200

    proc

--- resources/js/Components/dailyusage.vue
+++ resources/js/Components/dailyusage.vue
@@ -1,58 +1,49 @@
 <template>
     <div class="includes-h5">
-    <h5>  avarage current consumption</h5>
-    <div class="daily-cost-container">
-
-        <div class="price-div">
-
-            <h6> Daily usage</h6>
-            <p>12W</p>
-
+        <h5>avarage current consumption</h5>
+        <div class="daily-cost-container">
+            <div class="price-div">
+                <h6>Daily usage</h6>
+                <p>12W</p>
+            </div>
+            <div class="prev-cost">
+                <h6>Avarage usage</h6>
+                <p>13.2W</p>
+            </div>
         </div>
-        <div class="prev-cost">
-            <h6>Avarage usage </h6>
-            <p>13.2W</p>
-
-        </div>
-    </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "dailyusage"
-}
-
-
+    name: "dailyusage",
+};
 </script>
 
 <style scoped>
-.includes-h5{
-    max-width:400px;
+.includes-h5 {
+    max-width: 400px;
     margin: auto;
 }
-h5{
+h5 {
     margin-top: 80px;
     color: #fff;
-    
 }
-.daily-cost-container{
-    
+.daily-cost-container {
     height: 150px;
     display: flex;
     flex-wrap: wrap;
     align-items: center;
     justify-content: space-between;
-    color:white;
-    background-color: rgba(17, 172, 132, 0.50);
-    padding:20px;
-    border-radius:10px;
-    
+    color: white;
+    background-color: rgba(17, 172, 132, 0.5);
+    padding: 20px;
+    border-radius: 10px;
 }
 
-.daily-cost-container p{
+.daily-cost-container p {
     font-weight: lighter;
-    text-align: center
+    text-align: center;
 }
 
 /* 
@@ -61,5 +52,4 @@ h5{
        display: block;
     }
 } */
-
 </style>
